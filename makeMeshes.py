902dc80ecb9d205efb211f20e8ecff571e0c628b
Jacob Bieker <jacobbieker@users.noreply.github.com>
2018-05-12 19:58:19 -0700

    Add updated versions
    Creates the correct meshes for Neutrophyll and Gut scans

--- makeMeshes.py
+++ makeMeshes.py
@@ -8,10 +8,10 @@ import numpy as np
 
 
 #data2 = np.load("gut_outline_DIC.npz")['images']
-#data2 = np.load("intestinal_outline.npz")['images']
+data2 = np.load("intestinal_outline.npz")['images']
 
 
-data2 = np.load("rough_mask.npy")
+#data2 = np.load("rough_mask.npy")
 
 #zeroes = np.zeros(data2.shape)
 
@@ -30,14 +30,14 @@ data2 = np.load("rough_mask.npy")
 #non_zeros = np.transpose(np.nonzero(data2))
 # Display resulting triangular mesh using Matplotlib. This can also be done
 
-verts, faces, normals, values = measure.marching_cubes_lewiner(data2, spacing=(6,1,1))
+verts, faces, normals, values = measure.marching_cubes_lewiner(data2, spacing=(9,6,6))
 print("Verticies: " + str(len(verts)))
 
 #verts, faces, normals, values = measure.marching_cubes_lewiner(data1, 0)
 
 faces= faces+1
 
-thefile = open('C:\Development\GoogleVR_Testing\Assets\\neutrophyll.obj', 'w')
+thefile = open('fixed_gut_5.obj', 'w')
 for item in verts:
     thefile.write("v {0} {1} {2}\n".format(item[0],item[1],item[2]))
 
