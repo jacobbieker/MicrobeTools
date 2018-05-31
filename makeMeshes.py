bd6ca1c921592a0dc117ea3b813b097677ecbfed
Jacob Bieker <jacobbieker@users.noreply.github.com>
2018-05-31 12:08:29 -0700

    Start on adding commandline options

--- makeMeshes.py
+++ makeMeshes.py
@@ -1,50 +1,26 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-
 from skimage import measure
-from skimage.draw import ellipsoid
 
 import numpy as np
+import os
 
+@click.command()
+@click.argument('mask', type=click.Path(exists=True, dir_okay=False), help='Input Mask or video file')
+@click.argument('step_size', type=int, help='Step size for the Marching Cubes, higher values give lower resolution')
+@click.argument('spacing', type=tuple, help='Spacing of the slices in (Z,X,Y) format, defaults to (6,1,1)')
+@click.argument('-o', '--output', type=str, help='Output name for the OBJ file')
+@click.option('-v', '--video', help='Make mask from video input', is_flag=True)
+def make_mesh(mask, step_size, spacing, output, yes, video):
+    verts, faces, normals, values = measure.marching_cubes_lewiner(mask, spacing=spacing, step_size=step_size)
+    print("Verticies: " + str(len(verts)))
 
-#data2 = np.load("gut_outline_DIC.npz")['images']
-data2 = np.load("intestinal_outline.npz")['images']
+    thefile = open(output, 'w')
+    for item in verts:
+        thefile.write("v {0} {1} {2}\n".format(item[0],item[1],item[2]))
 
+    for item in normals:
+        thefile.write("vn {0} {1} {2}\n".format(item[0],item[1],item[2]))
 
-#data2 = np.load("rough_mask.npy")
+    for item in faces:
+        thefile.write("f {0}//{0} {1}//{1} {2}//{2}\n".format(item[0],item[1],item[2]))
 
-#zeroes = np.zeros(data2.shape)
-
-#for x in range(data2.shape[0]):
-#    for y in range(data2.shape[1]):
-#        for z in range(data2.shape[2]):
-#            if data2[x][y][z] == 2:
-#                zeroes[x][y][z] = 1
-
-# Now have a thing with only the ones
-
-
-
-#print(data1.files)
-#data2 = data1['images']
-#non_zeros = np.transpose(np.nonzero(data2))
-# Display resulting triangular mesh using Matplotlib. This can also be done
-
-verts, faces, normals, values = measure.marching_cubes_lewiner(data2, spacing=(9,6,6))
-print("Verticies: " + str(len(verts)))
-
-#verts, faces, normals, values = measure.marching_cubes_lewiner(data1, 0)
-
-faces= faces+1
-
-thefile = open('fixed_gut_5.obj', 'w')
-for item in verts:
-    thefile.write("v {0} {1} {2}\n".format(item[0],item[1],item[2]))
-
-for item in normals:
-    thefile.write("vn {0} {1} {2}\n".format(item[0],item[1],item[2]))
-
-for item in faces:
-    thefile.write("f {0}//{0} {1}//{1} {2}//{2}\n".format(item[0],item[1],item[2]))
-
-thefile.close()
+    thefile.close()
